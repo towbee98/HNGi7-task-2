9db9a03de84b7e900bbc131f10607e7faeba7dc4
Adaeze23 <nwaiwuodusadaeze@gmail.com>
2020-06-05 23:56:00 +0100

    swift_code HNG-00500 script

--- scripts/swift_code-HNG-00500.js
+++ scripts/swift_code-HNG-00500.js
@@ -1,10 +1,10 @@
 const bio = {
 
-fullname: "Adaeze NwaiwuOdus",
+fullName: "Adaeze Nwaiwu-Odus",
 id: "HNG-00500",
 email: "nwaiwuodusadaeze@gmail.com",
 language:"JavaScript"
 };
-let info =`Hello World, this is ${bio.fullname} with HNGi7 ID ${bio.id} and email ${bio.email} using ${bio.language} for stage 2 task`
+let info =`Hello World, this is ${bio.fullName} with HNGi7 ID ${bio.id} and email ${bio.email} using ${bio.language} for stage 2 task`
 
 console.log(info);
