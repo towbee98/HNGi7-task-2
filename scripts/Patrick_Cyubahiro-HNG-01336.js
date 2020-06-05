b34d3f178fcc7c8fe0231f6271e88619dff4c772
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2020-06-05 23:29:35 +0300

    Update Patrick_Cyubahiro-HNG-01336.js

--- scripts/Patrick_Cyubahiro-HNG-01336.js
+++ scripts/Patrick_Cyubahiro-HNG-01336.js
@@ -3,7 +3,6 @@ var id = "HNG-01336";
 var email = "ampatrickcyubahiro@gmail.com";
 var language = "JavaScript";
 
-console.log("Hello World, this is " + name + " with HNGi7 ID " + id " and email " + email + " using " + language + " for stage 2 task");
-
+console.log("Hello World, this is " + name + " with HNGi7 ID " + id + " and email " + email + " using " + language + " for stage 2 task");
 
 
