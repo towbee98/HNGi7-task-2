5e2ec3eaeb3f34eeacbbf6e92cd1de737d833b63
Adenekan Kofoworola <adenekankofoworola@gmail.com>
2020-06-06 14:12:49 +0100

    chnage id fornat HNG-00238

--- scripts/Kofoworola-HNG-00238.js
+++ scripts/Kofoworola-HNG-00238.js
@@ -1,5 +1,5 @@
-const name = "Adenekan Simbiat"
-const id = "00238";
+const name = "Adenekan Simbiat";
+const id = "HNG-00238";
 const language = "JavaScript";
-const email = "adenekankofoworola@gmail.com"
-console.log(`Hello World, this is ${name} with HNGi7 ID HNG-${id} and email ${email} using ${language} for stage 2 task`);
+const email = "adenekankofoworola@gmail.com";
+console.log(`Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
