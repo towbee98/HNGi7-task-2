9b4823d884eb40f98ad4e4ff22f76489e52ed17a
NmahJenny30 <66219359+NmahJenny30@users.noreply.github.com>
2020-06-06 17:10:24 +0100

    Nmah30-HNG-02057 script

---
+++ scripts/Nmah30-HNG-02057.js
@@ -0,0 +1,6 @@
+var myName = "Chidinma Chukwuma";
+var myID = "HNG-02057";
+var myMail = "chidinmachukwuma4@gmail.com";
+var myProgram = "JavaScript";
+var myResult = "Hello World, " + "this is " + myName + " with HNGi7 ID " + myID + " and email " + myMail + " using " + myProgram + " for stage 2 task";
+console.log(myResult);
