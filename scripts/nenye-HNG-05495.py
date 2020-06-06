e9b74aac1de22007107d6380a9a383b97f8e4d6d
Chukwunenye Okanumee <nenye.okanumee@gmail.com>
2020-06-06 10:45:39 +0100

    nenye 05495

---
+++ scripts/nenye-HNG-05495.py
@@ -0,0 +1,7 @@
+name = "Chukwunenye Okanumee"
+ID = "HNG-05495"
+email = "nenye.okanumee@gmail.com"
+language = "Python"
+
+print(f"Hello World, this is {name} with HNGi7 ID {ID} and email {email} using {language} for stage 2 task ")
+
