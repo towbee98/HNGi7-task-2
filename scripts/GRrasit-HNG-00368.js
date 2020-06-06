f46a0e3b9828c62ad0fedb0f8e95810fef98b92b
Grasit <idongesiteshiet980@gmail.com>
2020-06-06 09:20:49 +0100

    grasit-HNG-00368

--- scripts/GRrasit-HNG-00368.js
+++ scripts/GRrasit-HNG-00368.js
@@ -3,5 +3,5 @@ const ID = "HNG-00368";
 const email ="idongesiteshiet980@gmail.com";
 const language = "Javascript";
 const result =
- `Hello World, this is ${name} with HNGi7 ID ${ID}  and email ${email} using ${language} for stage 2 task`;
+ `Hello World, this is ${name} with HNGi7 ID ${ID} and email ${email} using ${language} for stage 2 task`;
 console.log(result);
