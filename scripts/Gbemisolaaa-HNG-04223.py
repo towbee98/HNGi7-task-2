154a9559596bd87779f108b9dd54dc7566e49444
unknown <yemmyaweda@gmail.com>
2020-06-06 19:30:42 +0100

    Gbemisolaaa-HNG-04223 script

---
+++ scripts/Gbemisolaaa-HNG-04223.py
@@ -0,0 +1,6 @@
+first_name = 'Gbemisola'
+last_name = 'Aweda'
+i_d = 'HNG-04223'
+e_mail = 'yemmyaweda@gmail.com'
+message = f'Hello World, this is {first_name} {last_name} with {i_d} and my email {e_mail} using Python for stage 2 task'
+print(message)
