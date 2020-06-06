a896d39e3afa81831d606b004c04992d2c6581b2
Scofield1 <61793402+Scofield1@users.noreply.github.com>
2020-06-06 09:02:10 -0700

    Scofield1-HNG-05656 script

---
+++ scripts/HNG-05656.js
@@ -0,0 +1,14 @@
+function taskTwo(name,intershipID,email){
+    this.name = name;
+    this.intershipID = intershipID;
+    this.email = email;
+    
+    this.detail = function(){
+        return "Hello World, this is " + this.name + " with " + this.intershipID + " and  email " + this.email + " using Javascript for stage 2 task"
+    }
+}
+
+var task = new taskTwo ("Abdulrauf Abiodun","HNG-05656","adebayoabdulrauf1@gmail.com");
+
+
+console.log(task.detail());
