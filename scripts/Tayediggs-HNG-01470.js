0ea77b6558cd43eff107c2c95445dea17a2eb4a1
tayediggs <61007777+tayediggs@users.noreply.github.com>
2020-06-06 06:30:39 +0100

    Update Tayediggs-HNG-01470.js
    updated

--- scripts/Tayediggs-HNG-01470.js
+++ scripts/Tayediggs-HNG-01470.js
@@ -4,6 +4,6 @@ const _lang = 'Javascript';
 const _email = 'abiolataiwo36@yahoo.com';
 
 function getinfo() {
-        return 'Hello World, this is ${_name} with HNGi7 ID ${_id} and email ${_email} using ${_lang} for stage 2 task'
+        return `Hello World, this is ${_name} with HNGi7 ID ${_id} and email ${_email} using ${_lang} for stage 2 task`
     } 
 console.log(getinfo())
