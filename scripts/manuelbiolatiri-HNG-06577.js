5c5763a2e91718ff4bba15cecadd77e4cfe70bab
manuelbiolatiri <manuellaide49@gmail.com>
2020-06-06 10:12:34 +0100

    manuelbiolatiri-HNG-06577

--- scripts/manuelbiolatiri-HNG-06577.js
+++ scripts/manuelbiolatiri-HNG-06577.js
@@ -3,4 +3,4 @@ let id = 'HNG-06577';
 let email = 'manuellaide49@gmail.com';
 let language = 'Javascript';
 
-console.log(`Hello world, this is ${name
+console.log(`Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task.`);
