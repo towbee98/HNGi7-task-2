e667c4a2570a40c856208f51102d64bdd4a62069
Wemitech <omololaoluwawemimo09@gmail.com>
2020-06-06 20:04:11 +0100

    Wemitech-HNG-05687

--- scripts/Wemi-HNG-05687.js
+++ scripts/Wemi-HNG-05687.js
@@ -1,15 +1,2 @@
-const _name = 'Omolola Oluwawemimo';
-const _id = 'HNG 05687';
-const _lang = 'Javascript';
-const _email = 'Oluwawemimog@gmail.com';
-
-function getinfo() {
-    return "Hello World, this is ${_name} with HNGi7 ID ${_id} and email ${_email} ${_lang} for task 2"
-}
-console.log(getinfo())
-
-
-
-
-
- 
+console.log( "Hello World, this is Omolola Oluwawemimo with HNGi7 ID HNG-05687 and email Oluwawemimog@gmail.com using Javascript for  stage task 2"
+    )
