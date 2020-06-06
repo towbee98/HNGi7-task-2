d6da4ad21683489634662473bda23a9338da5f2e
Frontliner <56607245+eomopelumi@users.noreply.github.com>
2020-06-06 19:42:44 +0100

    Update PelumiElusakin-HNG-05890.js

--- scripts/PelumiElusakin-HNG-05890.js
+++ scripts/PelumiElusakin-HNG-05890.js
@@ -5,4 +5,4 @@ let language = 'JavaScript';
 
 
 
-console.log("Hello World, this is " + fullname + " with HNGi7 ID " + ID + " and email " + email +  " using " + lang/uage + " for stage 2 task ");
+console.log("Hello World, this is " + fullname + " with HNGi7 ID " + ID + " and email " + email +  " using " + language + " for stage 2 task ");
