d47c762f4055b73151bc218c73fd532dc8f4328a
Jochoniah <muathanzomo1@gmail.com>
2020-06-06 20:51:52 +0300

    Jochoniah-HNGi7-HNG-01516.js

--- scripts/Jochoniah-HNGi7-HNG-01516.js
+++ scripts/Jochoniah-HNGi7-HNG-01516.js
@@ -8,14 +8,11 @@ let fullName = firstName+ ' ' + lastName;
 // set HNGi7 ID 
 let HNGi7_ID = "HNG-01516"; 
 
-// email
-let email = "muathanzomo1@gmail.com"
-
 // langauge
 
 let language = "JavaScript";
 
-let result = `Hello World, this is ${fullName} with HNGi7 ID ${HNGi7_ID} and email ${email } using ${language} for stage 2 task`;
+let result = `Hello World, this is ${fullName} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2 task`;
 console.log(result);
 
 
