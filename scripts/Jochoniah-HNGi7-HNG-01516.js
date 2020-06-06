493a0b6bcfcc432717bce1b59ea0041e49788132
Jochoniah <muathanzomo1@gmail.com>
2020-06-06 11:45:17 +0300

    Jochoniah-HNGi7-HNG-01516.js

---
+++ scripts/Jochoniah-HNGi7-HNG-01516.js
@@ -0,0 +1,18 @@
+// Intern name
+let firstName = "Jochoniah";
+let lastName = "Nzomo";
+
+// full name
+let fullName = firstName+ ' ' + lastName;
+
+// set HNGi7 ID 
+let HNGi7_ID = "HNG-01562"; 
+
+// langauge
+
+let language = "JavaScript";
+
+let result = `Hello World, this is ${fullName} with HNGi7 ID ${HNGi7_ID} using ${language} for stage 2 task`;
+console.log(result);
+
+
