63781bee9c4910bdf50c8879d9a0c71e783dca40
KwennB <60541622+kwennB@users.noreply.github.com>
2020-06-06 09:37:22 +0100

    Update KwennB-HNG-01909.js
    FINAL CORRECTION

--- scripts/KwennB-HNG-01909.js
+++ scripts/KwennB-HNG-01909.js
@@ -1,7 +1,7 @@
-let fullName = "Anyebe Blessing Omotayo";
-let hngId = "01909";
+let fullName = "Anyebe Blessing";
+let hngId = "HNG-01909";
 let language = "JavaScript"
 let email = "anyebeblessing737@yahoo.com"
 
 
-console.log(`Hello  World, this is ${fullName} with HNG ID ${hngId} and email ${email} using ${language} for stage 2 task`);
+console.log(`Hello World, this is ${fullName} with HNG ID ${hngId} and email ${email} using ${language} for stage 2 task`);
