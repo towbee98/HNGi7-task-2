a1bba8dcad63bfafcc40ee73324591132849ec94
temitope akinmegha <48892912+temitopeakin1@users.noreply.github.com>
2020-06-06 01:19:00 +0100

    Update Tmegha-HNG-00773.js
    Updated the script name

--- scripts/T'megha-HNG-00773.js
+++ scripts/T'megha-HNG-00773.js
@@ -1,7 +1,10 @@
-const greetings = 'Hello World';
-const name = "Akinmegha Temitope";
-const id = 'HNG-00773';
-const language = 'JavaScript';
-const email = 'temitopeakinmegha@gmail.com';
-
-console.log(`${greetings}, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
+const introduction = ({name, id, email, language}) =>{ 
+    return `Hello World, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`
+   }
+   const myIntroduction = introduction({
+    name: 'Akinmegha Temitope',
+    id:  'HNG-00773',
+    email: 'temitopeakinmegha@gmail.com',
+    language: 'JavaScript',
+   });
+   console.log(myIntroduction);
