db4987708669eb3e7aa04c2caf4bed6227dd3ad0
Greennsikak <57267256+Greennsikak@users.noreply.github.com>
2020-06-06 08:48:17 +0100

    Update krowniey-HNG-01306.js

--- scripts/krowniey-HNG-01306.js
+++ scripts/krowniey-HNG-01306.js
@@ -4,4 +4,4 @@ let  id = 'HNG-01306';
 let email = 'comfortowo4@gmail.com'
 let display =  greeting + ' this is ' + name + ' with HNGi7 ID '+ id + ' and email '+ email
  +' using JavaScript for stage 2 task';
-return console.log(display);
+console.log(display);
