79a9687d93a4736497e5c344e39dbb3a1ce6c7a4
iamkingOz <ozokweluvic@gmail.com>
2020-06-06 09:42:16 +0100

    modified script 2

--- scripts/Victor-HNG-02089.js
+++ scripts/Victor-HNG-02089.js
@@ -1,11 +1,11 @@
 function hello() {
 
-  let fullname = "ozokwelu victor"; // fullname
+  let fullname = "Ozokwelu Victor"; // fullname
   const ID = "HNG-02089";//intership ID
   let language = "javascript";//prefered language
   const mail = "ozokweluvic@gmail.com";//email
   console.log(
-    `Hello World, this is ${fullname} with HNGi7 ID ${ID} and email ${e_mail} using ${language} for stage 2 task`
+    `Hello World, this is ${fullname} with HNGi7 ID ${ID} and email ${mail} using ${language} for stage 2 task`
   )
 
 }
