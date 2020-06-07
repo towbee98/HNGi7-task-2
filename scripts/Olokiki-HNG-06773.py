dcfba2cd9168271465ec3a178671e6dbbf3bc5ea
Tomiwa97 <ajayitomiwa71@gmail.com>
2020-06-07 01:20:06 +0100

    File Updates

--- scripts/Olokiki-HNG-06773.py
+++ scripts/Olokiki-HNG-06773.py
@@ -3,5 +3,5 @@ my_id = 'HNG-06773'
 email = 'enibusola@gmail.com'
 language= 'Python'
 
-output = "Hello world, this is " + name + "with HNGi7 ID " + my_id + " and email " + email + " for stage 2 task"
+output = "Hello World, this is " + name + " with HNGi7 ID " + my_id + " and email " + email + " for stage 2 task"
 print(output)
